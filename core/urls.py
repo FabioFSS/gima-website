e955ef3428d61a04c799581648a16f64b2e6df4f
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-06-05 15:09:17 -0300

    added pages projetos minicursos and eventos

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, colaboradores, contato
+from .views import index, colaboradores, contato, minicursos, eventos, projetos
 
 urlpatterns = [
     path('', index, name='index'),
     path('colaboradores/', colaboradores, name='colaboradores'),
-    path('contato/', contato, name='contato')
+    path('contato/', contato, name='contato'),
+    path('minicursos/', minicursos, name='minicursos'),
+    path('eventos/', eventos, name='eventos'),
+    path('projetos/', projetos, name='projetos'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,12 @@ def colaboradores(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def minicursos(request):
+    return render(request, 'minicursos.html')
+
+def eventos(request):
+    return render(request, 'eventos.html')
+
+def projetos(request):
+    return render(request, 'projetos.html')
